ba28495e3745ae8a4d062ec9f01d5771d7217feb
Mat Sepahi <matt-fs@protonmail.com>
2019-08-25 15:09:30 +0000

    revert bad changes

--- lib/Job/Async/Worker/Redis.pm
+++ lib/Job/Async/Worker/Redis.pm
@@ -67,6 +67,10 @@ async sub on_job_received {
         $log->debugf("Current job count is %d", $job_count);
         $self->trigger;
         my ($items) = await $self->redis->hgetall('job::' . $id);
+        $self->redis->hmset(
+            'job::' . $id,
+            _started => Time::HiRes::time()
+        )->retain;
         my %data = @$items;
         my $result = delete $data{result};
         $log->debugf('Original job data is %s', \%data);
@@ -86,7 +90,11 @@ async sub on_job_received {
                     delete $self->{pending_jobs}{$id};
                     $log->tracef('Removing job from processing queue');
                     return Future->needs_all(
-                        $tx->hmset('job::' . $id, result => ref($rslt) ? 'J' . encode_json_utf8($rslt) : 'T' . $rslt),
+                        $tx->hmset(
+                            'job::' . $id,
+                            _processed => Time::HiRes::time(),
+                            result => ref($rslt) ? 'J' . encode_json_utf8($rslt) : 'T' . $rslt
+                        ),
                         $tx->publish('client::' . $data{_reply_to}, $id),
                         $tx->lrem(
                             $self->prefixed_queue($self->processing_queue) => 1,
